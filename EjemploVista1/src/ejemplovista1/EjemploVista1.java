57c351794f3bb108aed464bbd7e5ba49c42e8abc
ledesmarocio <rledesma2611@hotmail.com>
2023-08-11 22:23:02 -0300

    Ejercicios
    Ejemplo del video + 2 primeros ejercicios

---
+++ EjemploVista1/src/ejemplovista1/EjemploVista1.java
@@ -0,0 +1,9 @@
+package ejemplovista1;
+
+public class EjemploVista1 {
+
+    public static void main(String[] args) {
+       
+    }
+
+}
---
+++ Ejercicio2/src/ejercicio2/Ejercicio2.java
@@ -0,0 +1,9 @@
+package ejercicio2;
+
+public class Ejercicio2 {
+
+    public static void main(String[] args) {
+       
+    }
+
+}
